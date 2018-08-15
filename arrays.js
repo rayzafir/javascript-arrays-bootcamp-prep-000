aa57e548219ac0b69c1f4f5354d932a50a2f36c2
Ray Raven <rayzafir@aol.com>
2018-08-15 22:53:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'] 
 
-var addElementTobeginningOfArray= ['1','2','3']{
-addElementTobeginningOfArray.unshift(array, element)
+function addElementTobeginningOfArray (array,element)
+return [array,...element]
 }
